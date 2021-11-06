044b5298db7f7ea6c4dbde847549b9102bd57e41
yuvalpruss <yuval@wenrix.com>
2021-11-06 19:58:57 +0200

    Change migration

--- migrations/migrate_file.py
+++ migrations/migrate_file.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from enums import Status
 from apis.parking_lot import ParkingLotApi
+from utils.time_serialize import datetime_to_str
 
 FILE = "/home/ubuntu/parking2/results.txt"
 
@@ -17,8 +18,7 @@ if __name__ == "__main__":
         query_time = datetime.strptime(line.strip().split("|")[1], "%m/%d/%Y, %H:%M:%S")
 
         if current_status != Status.unknown:
-            status = {str(Status.empty.value): 0, str(Status.few_left.value): 0, str(Status.full.value): 0}
-            status[str(current_status.value)] += 1
+            status = {datetime_to_str(query_time)}
             task = ParkingLotApi.create_status(lot_id=45,
                                                day=query_time.weekday(),
                                                hour=query_time.hour,

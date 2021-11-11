85ffb5f48acab052b4baff33475e64cb6304df94
yuvalpruss <yuval@wenrix.com>
2021-11-11 22:48:13 +0200

    Add lots table

--- initial_db.sql
+++ initial_db.sql
@@ -8,3 +8,18 @@ create table if not exists parking_lots (
     minute INT NOT NULL,
     status JSONB
 );
+
+create table if not exists lots (
+  id SERIAL PRIMARY KEY,
+  lot_id INT NOT NULL,
+  eng_name TEXT NOT NULL,
+  heb_name TEXT NOT NULL,
+  address TEXT NOT NULL,
+  nicknames JSONB
+);
+
+INSERT INTO lots (lot_id, eng_name, heb_name, address, nicknames) VALUES (40, 'ravnitzky', 'רבניצקי', 'רבניצקי 6 תל-אביב יפו', '[]');
+INSERT INTO lots (lot_id, eng_name, heb_name, address, nicknames) VALUES (94, 'habima plaza', 'התרבות', ' הוברמן 1 תל-אביב יפו', '["הבימה"]');
+INSERT INTO lots (lot_id, eng_name, heb_name, address, nicknames) VALUES (45, 'tel nordau', 'תל-נורדאו', 'פרישמן 28 תל-אביב יפו', '[]');
+
+INSERT INTO lots (lot_id, eng_name, heb_name, address, nicknames) VALUES (31, 'Da Vinci ', 'מפעל הפיס', 'ליאונרדו דוינצי 5 תל-אביב יפו', '["Hapais"]');
--- models/parking_lot.py
+++ models/parking_lot.py
@@ -1,7 +1,17 @@
-from sqlalchemy import Column, JSON, Integer
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, JSON, Integer, String, ARRAY
 
 from models.base import Model
+
+
+class Lot(Model):
+    __tablename__ = "lots"
+
+    id: int = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    lot_id: int = Column(Integer(), unique=True, nullable=False)
+    eng_name: str = Column(String(), nullable=False)
+    heb_name: str = Column(String(), nullable=False)
+    address: str = Column(String(), nullable=False)
+    nicknames: list[str] = Column(ARRAY, nullable=True)
 
 
 class ParkingLot(Model):
@@ -18,4 +28,5 @@ class ParkingLot(Model):
     status: dict = Column(JSON(), nullable=False, default={})
 
 
+LotTable = Lot.__table__
 ParkingLotTable = ParkingLot.__table__

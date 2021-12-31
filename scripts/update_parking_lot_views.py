4bb378a8a71222274081fe887cf026b67b4c512c
yuvalpruss <yuval@wenrix.com>
2021-12-31 17:18:32 +0200

    Fix heat map calculation

--- scripts/update_parking_lot_views.py
+++ scripts/update_parking_lot_views.py
@@ -1,19 +1,27 @@
 import asyncio
-from collections import defaultdict
+from dataclasses import dataclass
 
 from apis.parking_lot_view import ParkingLotViewApi
 from enums import Status, STATUS_TO_SCORE
 from lot_ids import LOT_IDS
 
 from apis.parking_lot import ParkingLotApi
-from utils.time_serialize import time_with_dow_to_str
+
+
+@dataclass
+class HourScore:
+    score_sum: float = 0.0
+    score_count: float = 0.0
+
+    def get_score_avg(self) -> float:
+        return round(self.score_sum / self.score_count, 2)
 
 
 async def update_parking_lot_views():
     for lot_id in LOT_IDS:
         results = await ParkingLotApi.get_parking_lot_full_data(lot_id=lot_id)
 
-        heat_map_data = defaultdict(float)
+        heat_map_data = dict()
 
         for result in results:
             dict_result = dict(result)
@@ -27,10 +35,21 @@ async def update_parking_lot_views():
                     score += status_score
                     total += 1
 
-            heat_map_data[time_with_dow_to_str(dict_result["day"], dict_result["hour"], dict_result["minute"])] = (
-                round(score / total, 2)
-            )
+            day = dict_result["day"]
+            hour = dict_result["hour"]
 
+            if day not in heat_map_data:
+                heat_map_data[day] = {}
+
+            if hour not in heat_map_data[day]:
+                heat_map_data[day][hour] = HourScore()
+
+            heat_map_data[day][hour].score_sum += score
+            heat_map_data[day][hour].score_sum += total
+
+        for day, day_data in heat_map_data.items():
+            for hour, hour_data in day_data.items():
+                hour_data[hour] = hour_data[hour].get_score_avg()
         await ParkingLotViewApi.create_view(lot_id, dict(heat_map_data))
 
 if __name__ == "__main__":
--- utils/time_serialize.py
+++ utils/time_serialize.py
@@ -7,7 +7,3 @@ def datetime_to_str(d: datetime) -> str:
 
 def str_to_datetime(s: str) -> datetime:
     return datetime.strptime(s, '%d/%m/%Y %H:%M')
-
-
-def time_with_dow_to_str(day: int, hour: int, minute: int) -> str:
-    return f"{day} {hour}:{minute}"
